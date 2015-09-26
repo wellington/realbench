94bc46305e0b4aeebb571e35706ee49b763e1a0a
Drew Wells <drew.wells00@gmail.com>
2015-09-26 10:12:33 -0500

    hold gulp until the job is done

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,29 @@ gulp.task('post', function () {
     var postcss = require('gulp-postcss');
     var syntax = require('postcss-scss');
     var rename = require('gulp-rename');
-    var partimport = require('postcss-partial-import')
-    var plugins = [partimport({ extension: "scss"})];
 
-    gulp.src('short/*.scss')
+    var plugins = [
+        require('postcss-partial-import')({ extension: "scss"}),
+        // These throw errors on bootstrap-sass
+        // require('postcss-sass-mixins'),
+        // require('postcss-mixins'),
+        // require('postcss-nested'),
+        // require('postcss-simple-vars'),
+        // require('postcss-calc'),
+    ];
+
+    return gulp.src('short/*.scss')
             .pipe( postcss(plugins, { parser: require('postcss-scss') }) )
             .pipe(rename(function(path){
                 path.extname = '.css';
             }))
-            .pipe( gulp.dest('build/') )
+        .pipe( gulp.dest('build/') )
+        .on('error', function(e) { console.log(e.message); })
 });
 
 gulp.task('sass', function () {
     var sass = require('gulp-sass');
-    gulp.src('short/*.scss')
+    return gulp.src('short/*.scss')
         .pipe(sass.sync().on('error', sass.logError)
               .on('done', function(){ console.log(arguments); }))
         .pipe(gulp.dest('build/'))
